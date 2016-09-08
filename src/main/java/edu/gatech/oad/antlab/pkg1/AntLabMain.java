fb9b32b2fc5b521c0259aed6e3a07f25d8b743e2
emilylan <emilyynguyen@hotmail.com>
2016-09-08 14:49:55 -0400

    Update AntLabMain.java

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,8 +73,8 @@ import edu.gatech.oad.antlab.pkg2.*;
           toPrint += p4.toString("mravichandran6");
           //Person5 replace P4 with your name
           //and gburdell5 with your gt id
-          Person5 p5 = new Person5("P5");
-          toPrint += p5.toString("gburdell5");
+          Person5 p5 = new Person5("Emily Nguyen");
+          toPrint += p5.toString("enguyen9");
 		  
         System.out.println(toPrint);
         
